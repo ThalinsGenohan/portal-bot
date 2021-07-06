213dd6dc584af4c33fd4f721c93a1214fce0a074
Thalins <thalinsgenohan@gmail.com>
2021-07-06 05:06:13 -0400

    Add string format

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const Bot = require("./Bot");
 
+if (!String.prototype.format) {
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined'
+				? args[number]
+				: match;
+		});
+	};
+}
+
 main();
 
 async function main() {
